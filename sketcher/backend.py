02272bc1bff0e6d8493a1517eb79f1f736378bd1
Theo Cavignac <theo.cavignac@ecl16.ec-lyon.fr>
2019-01-08 10:25:29 +0100

    Add a error when no backend are found.

--- sketcher/backend.py
+++ sketcher/backend.py
@@ -1,3 +1,16 @@
+supported_backends = [
+    "tkinter",
+]
+
+
+class NoBackend(BaseException):
+    def __init__(self):
+        super().__init__(
+            "There is no supported graphical backend installed.\n" +
+            "Try to install one of those:\n" +
+            "\n".join("- " + back for back in supported_backends)
+        )
+
 try:
     import tkinter as tk
     have_tk = True
@@ -6,3 +19,5 @@ except:
 
 if have_tk:
     from .backend_tk import Backend
+else:
+    raise NoBackend()
